525139fcda112fda105392d0c87e92389625c6bf
Diki Andriansyah <diki1aap@gmail.com>
2016-01-21 19:39:26 +0700

    [experiment] try travis

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/posts')
+        $this->visit('posts')
              ->see('Hello Hi ...');
     }
 }
