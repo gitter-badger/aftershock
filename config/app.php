b0d56cf539511be3165d17af3f9876933621c27e
Diki Andriansyah <diki1aap@gmail.com>
2016-03-09 07:11:20 +0700

    Backup

--- config/app.php
+++ config/app.php
@@ -149,7 +149,7 @@ return [
          * 3rd party Service Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
-        // Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
     ],
 
@@ -205,7 +205,7 @@ return [
          */
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
-        // 'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
 
     ],
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,8 +8,11 @@
 	<link rel="icon" type="image/x-icon" href="{{ url() }}/img/favicon.ico" />
     <link rel="icon" href="{{ url() }}/img/favicon.png" />
     <link rel="apple-touch-icon" href="{{ url() }}/img/favicon.png" />
-
+  	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet'
+        type='text/css'>
+  	<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="{{ url() }}/css/vendor.css">
+	<style>body{font-family:'Lato';}.fa-btn{margin-right:6px;}</style>
   	<script src="{{ url() }}/js/jquery.min.js"></script>
   	<script src="{{ url() }}/js/bootstrap.min.js"></script>
 </head>
